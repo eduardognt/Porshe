1043127cdddec523faac00f211f81f38e9816329
Eduardo Gentilini <eduardo.gentilini.pro@gmail.com>
2026-07-07 17:25:52 -0300

    prepare to next step

---
+++ porsche/src/Components/Colors.jsx
@@ -0,0 +1,35 @@
+function Colors() {
+  const colors = [
+    {
+      id: "purple",
+      name: "Purple Grape",
+      image: "../src/assets/images/Porsche Taycan ROXA.jfif",
+      colorClass: "bg-purple-500",
+    },
+    {
+      id: "green",
+      name: "Green Max",
+      image: "../src/assets/images/Porsche Taycan VERDE.jfif",
+      colorClass: "bg-green-700",
+    },
+    {
+      id: "gray",
+      name: "Gray Inox",
+      image: "../src/assets/images/PGRAY.jfif",
+      colorClass: "bg-gray-500",
+    },
+  ];
+
+  const models = [
+    { name: "Taycan", motor: "460cv Hybrid", model: "2026" },
+    { name: "911 s", motor: "711cv Turbo", model: "25/26" },
+  ];
+
+  return (
+    <div>
+      <h2>Teste</h2>
+    </div>
+  );
+}
+
+export default Colors;
